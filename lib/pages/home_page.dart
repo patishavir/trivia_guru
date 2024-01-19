13cb25e6afc6ebd90abd38cf548143cdba5b2f2f
patishavir <patishavir@gmail.com>
2024-01-20 01:11:42 +0200

    trivia_guru commit 20240120

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trivia_guru/pages/home_page_widgets.dart';
 import '../common/logging_utils.dart';
 import '../config/game_config.dart';
 import '../config/session_data.dart';
@@ -11,17 +12,16 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   late Question question;
   bool isCorrectAnswer = false;
 
   void processAnswer(int i) {
     setState(
-          () {
+      () {
         SessionData.selectedAnswer = i + 1;
         SessionData.waitingForAnAnswer = false;
         isCorrectAnswer =
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     } else {
       setState(
-            () {
+        () {
           SessionData.waitingForAnAnswer = true;
           SessionData.questionIndex++;
           SessionData.selectedAnswer = 0;
@@ -67,7 +67,9 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Scaffold(
         appBar: AppBar(
           // title: Text(widget.title),
-          title: Text( AppLocalizations.of(context)!.app_title,),
+          title: Text(
+            AppLocalizations.of(context)!.app_title,
+          ),
           centerTitle: true,
         ),
         body: Center(
@@ -89,148 +91,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Widget> getWidgetList() {
     List<Widget> widgetList = [];
-    List<Widget> answerButtonList = [];
 
     if (question.qimage != null && question.qimage!.isNotEmpty) {
-      widgetList.add(
-        Center(
-          child: SizedBox(
-            width: 200.0,
-            height: 200.0,
-            child: Image.asset('assets/images/${question.qimage}',
-                fit: BoxFit.fill),
-          ),
-        ),
-      );
+      widgetList.add(getQuestionImage(question));
     }
     widgetList.add(
-      Container(
-        margin: const EdgeInsets.all(4.0),
-        padding: const EdgeInsets.all(4.0),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 0, 0, 255),
-          border: Border.all(color: Colors.black, width: 1.0),
-        ),
-        child: Text(
-          question.question,
-          style: const TextStyle(
-              fontSize: GameConfig.fontSize, color: Colors.white),
-        ),
-      ),
+      getQuestion(question),
     );
     // next question container
-    widgetList.add(Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SessionData.waitingForAnAnswer
-            ? Text(
-          AppLocalizations.of(context)!.select_an_answer,
-          style: const TextStyle(
-            fontSize: GameConfig.fontSize,
-            fontWeight: FontWeight.bold,
-            color: Color.fromARGB(255, 0, 0, 255),
-            backgroundColor: Colors.white,
-          ),
-        )
-            : ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor:
-            const MaterialStatePropertyAll<Color>(Colors.white),
-            elevation: MaterialStateProperty.resolveWith<double>(
-                  (Set<MaterialState> states) {
-                if (states.contains(MaterialState.pressed)) {
-                  return 10.0;
-                } else {
-                  return 10.0;
-                }
-              },
-            ),
-          ),
-          child: Text(
-            '${AppLocalizations.of(context)!.next_question} >',
-            style: const TextStyle(
-                fontSize: GameConfig.fontSize,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 0, 0, 255)),
-          ),
-          onPressed: () {
-            processNextButton();
-          },
-        ),
-        const Spacer(),
-        Text('${AppLocalizations.of(context)!.score}: ', style: Theme.of(context).textTheme.bodyLarge),
-        Text(
-          SessionData.correctAnswers.toString(),
-          style: const TextStyle(
-              fontSize: GameConfig.fontSize,
-              backgroundColor: Colors.green,
-              color: Colors.white),
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        Text(
-          SessionData.wrongAnswers.toString(),
-          style: const TextStyle(
-              fontSize: GameConfig.fontSize,
-              backgroundColor: Colors.red,
-              color: Colors.white),
-        ),
-        const Spacer(),
-      ],
-    ),
+    widgetList.add(
+      getSelectAnAnswerRow(question, context, this),
     );
-    answerButtonList.clear();
-    List<String> answers = [
-      question.answer1,
-      question.answer2,
-      question.answer3,
-      question.answer4
-    ];
-    for (int i = 0; i < GameConfig.answersCount; i++) {
-      LoggingUtils.writeLog('iteration number: $i');
-      Color? buttonColor = Colors.grey[400];
-      if (i == SessionData.selectedAnswer - 1) {
-        buttonColor = isCorrectAnswer ? Colors.green : Colors.red;
-      }
-      answerButtonList.add(
-        Container(
-          margin: const EdgeInsets.all(10.0),
-          child: TextButton(
-            onPressed: SessionData.waitingForAnAnswer
-                ? () {
-              processAnswer(i);
-            }
-                : null,
-            style: TextButton.styleFrom(
-                backgroundColor: buttonColor, shadowColor: buttonColor),
-            child: Text(
-              answers[i],
-              style: const TextStyle(
-                  color: Colors.black, fontSize: GameConfig.fontSize),
-            ),
-          ),
-        ),
-      );
-      widgetList.add(answerButtonList[i]);
-    }
+    widgetList.addAll(getAnswers(question, this, isCorrectAnswer));
 
     // add divider
-    widgetList.add(const Divider(
-      color: Colors.red,
-      height: 5.0,
-      thickness: 2.0,
-    ));
+    widgetList.add(
+      const Divider(
+        color: Colors.red,
+        height: 5.0,
+        thickness: 2.0,
+      ),
+    );
     // add answer text
     if (!SessionData.waitingForAnAnswer) {
-      widgetList.add(
-        Container(
-            margin: const EdgeInsets.all(10.0),
-            child: Text(question.answertext,
-                style: Theme.of(context).textTheme.bodyLarge)),
-      );
+      widgetList.add(getAnswerText(question, context));
     }
-
     return widgetList;
   }
 }
---
+++ lib/pages/home_page_widgets.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import '../common/logging_utils.dart';
+import '../config/game_config.dart';
+import '../config/session_data.dart';
+import '../objects/question.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+Widget getQuestionImage(Question question) {
+  return Center(
+    child: SizedBox(
+      width: 200.0,
+      height: 200.0,
+      child: Image.asset('assets/images/${question.qimage}', fit: BoxFit.fill),
+    ),
+  );
+}
+
+Widget getQuestion(Question question) {
+  return Container(
+    margin: const EdgeInsets.all(4.0),
+    padding: const EdgeInsets.all(4.0),
+    decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 0, 0, 255),
+      border: Border.all(color: Colors.black, width: 1.0),
+    ),
+    child: Text(
+      question.question,
+      style:
+          const TextStyle(fontSize: GameConfig.fontSize, color: Colors.white),
+    ),
+  );
+}
+
+Widget getSelectAnAnswerRow(
+    Question question, BuildContext context, MyHomePageState myHomePageState) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      SessionData.waitingForAnAnswer
+          ? Text(
+              AppLocalizations.of(context)!.select_an_answer,
+              style: const TextStyle(
+                fontSize: GameConfig.fontSize,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 0, 0, 255),
+                backgroundColor: Colors.white,
+              ),
+            )
+          : ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    const MaterialStatePropertyAll<Color>(Colors.white),
+                elevation: MaterialStateProperty.resolveWith<double>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return 10.0;
+                    } else {
+                      return 10.0;
+                    }
+                  },
+                ),
+              ),
+              child: Text(
+                '${AppLocalizations.of(context)!.next_question} >',
+                style: const TextStyle(
+                    fontSize: GameConfig.fontSize,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 0, 0, 255)),
+              ),
+              onPressed: () {
+                myHomePageState.processNextButton();
+              },
+            ),
+      const Spacer(),
+      Text('${AppLocalizations.of(context)!.score}: ',
+          style: Theme.of(context).textTheme.bodyLarge),
+      Text(
+        SessionData.correctAnswers.toString(),
+        style: const TextStyle(
+            fontSize: GameConfig.fontSize,
+            backgroundColor: Colors.green,
+            color: Colors.white),
+      ),
+      const SizedBox(
+        width: 8,
+      ),
+      Text(
+        SessionData.wrongAnswers.toString(),
+        style: const TextStyle(
+            fontSize: GameConfig.fontSize,
+            backgroundColor: Colors.red,
+            color: Colors.white),
+      ),
+      const Spacer(),
+    ],
+  );
+}
+
+List<Widget> getAnswers(
+    Question question, MyHomePageState myHomePageState, bool isCorrectAnswer) {
+  List<Widget> answerButtonList = [];
+  answerButtonList.clear();
+  List<String> answers = [
+    question.answer1,
+    question.answer2,
+    question.answer3,
+    question.answer4
+  ];
+  for (int i = 0; i < GameConfig.answersCount; i++) {
+    LoggingUtils.writeLog('iteration number: $i');
+    Color? buttonColor = Colors.grey[400];
+    if (i == SessionData.selectedAnswer - 1) {
+      buttonColor = isCorrectAnswer ? Colors.green : Colors.red;
+    }
+    answerButtonList.add(
+      Container(
+        margin: const EdgeInsets.all(10.0),
+        child: TextButton(
+          onPressed: SessionData.waitingForAnAnswer
+              ? () {
+                  myHomePageState.processAnswer(i);
+                }
+              : null,
+          style: TextButton.styleFrom(
+              backgroundColor: buttonColor, shadowColor: buttonColor),
+          child: Text(
+            answers[i],
+            style: const TextStyle(
+                color: Colors.black, fontSize: GameConfig.fontSize),
+          ),
+        ),
+      ),
+    );
+  }
+  return answerButtonList;
+}
+
+Widget getAnswerText (Question question, BuildContext context) {
+  return  Container(
+      margin: const EdgeInsets.all(10.0),
+      child: Text(question.answertext,
+          style: Theme.of(context).textTheme.bodyLarge),
+  );
+}
